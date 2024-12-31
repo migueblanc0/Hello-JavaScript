deb5c50bcd2bce4894a6b77dccd74757a348411a
miguezzb <miguezzblanco@gmail.com>
2024-12-30 20:32:15 -0600

    01-variables

---
+++ 01-variables.js
@@ -0,0 +1,23 @@
+//! LOWERCAMELCASE
+
+//var: origen de las variables en javascript
+var helloWorld = "¡Hello, JavaScript!";
+console.log(helloWorld);
+
+helloWorld = "¡Hola de nuevo, JavaScript!";
+console.log(helloWorld);
+
+//let
+let helloWorld2 = "¡Hello, JavaScript 2!";
+console.log(helloWorld2);
+
+helloWorld2 = "¡Hola de nuevo, JavaScript 2!";
+console.log(helloWorld2);
+
+//const: su valor es contante, no puede ser reasignada
+const helloWorld3 = "¡Hello, JavaScript 3!";
+console.log(helloWorld3);
+
+//ERROR
+//helloWorld3 = "¡Hola de nuevo, JavaScript 3!";
+//console.log(helloWorld3);
